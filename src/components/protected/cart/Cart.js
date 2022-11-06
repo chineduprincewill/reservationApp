1a7848aebd891ace9f3bdb5d9e4bc259d2a20deb
chinedu.ijeomah <chinedu.ijeomah@yahoo.com>
2022-11-06 21:15:00 +0100

    cart error fix

--- src/components/protected/cart/Cart.js
+++ src/components/protected/cart/Cart.js
@@ -64,8 +64,8 @@ const Cart = () => {
                 }
             );
             
-            console.log(response.data.cart_history);
-            setCart(response.data.cart_history);
+            console.log(response.data.cart);
+            setCart(response.data.cart);
             setCartcode(response.data.status_code);
 
             console.log(cartcode);
