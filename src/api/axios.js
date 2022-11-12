d5a9171aafb0243a88228bc70c5ba8f01002ecbd
chinedu.ijeomah <chinedu.ijeomah@yahoo.com>
2022-11-12 16:22:43 +0100

    change backend endpoint

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export default axios.create({
     //baseURL: 'https://lcm-project.herokuapp.com/api/admin'
-    baseURL: 'https://lcm.viceodev.tech'
+    baseURL: 'https://lcm.viceodev.tech/api/admin'
 });
